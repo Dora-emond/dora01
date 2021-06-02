7fec15f73e172f309ae9c293776d3a5948a6fbac
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-06-02 16:46:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['6289601145546','6283115810116'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -8,12 +8,12 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://api.xteam.xyz': 'Doraemondnobita123'
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'Kuharap kamu\'bisa bahagia'
+global.author = 'Dora:D'
 
 global.multiplier = 69 // The higher, The harder levelup
 
