316bd1c48bf98cac4aa81c5652307b71dcf141ca
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-06-12 03:29:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Kuharap kamu\'bisa bahagia'
+global.packname = 'Kuharap kamu\bisa bahagia'
 global.author = 'Dora:D'
 
 global.multiplier = 69 // The higher, The harder levelup
